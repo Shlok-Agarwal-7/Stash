fe1b5895b8eddde15a21e8ba40bf72b7bb3b3099
shlok <shlok.agarwal0604@gmail.com>
2025-09-28 19:49:43 +0530

    added assests and css utilities

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen">
+      <section className=" bg-surface-a10 hidden w-1/2 lg:flex xl:w-2/5">
+        <div className="flex-center flex-col space-y-5">
+          <div className="space-y-3 px-10 mt-6">
+            <h1 className="text-6xl italic text-primary-a0"> STASH </h1>
+            <h1 className="h1">Manage your files effortlessly</h1>
+            <p className="body-1">
+              Stay organized and boost your productivity.
+            </p>
+          </div>
+
+          <Image
+            src="/assets/images/files.png"
+            alt="Files Illustration"
+            width={342}
+            height={342}
+            className="transition-all hover:scale-105 hover:rotate-6"
+          />
+        </div>
+      </section>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
